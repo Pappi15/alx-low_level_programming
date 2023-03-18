eac0d40255055510ef6485b4ff95e63041e67fb2
Okonkwo Paschal Chiemerie <chi15emerie@gmail.com>
2023-03-18 04:54:57 -0700

    Created 5-printf.c file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - Print a string to standard output
+ * main - Prints a string to standard output
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
-{printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
 }
