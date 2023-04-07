17125c18341da748a3f6ca7ba18e1633bef0542c
Okonkwo Paschal Chiemerie <chi15emerie@gmail.com>
2023-04-06 17:57:57 -0700

    what

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,3 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - size
+ *  @s: pointer to string params
+ *  Return: recursion
+ */
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(++s));
+}
 
 /**
  * p1 - palindrome
@@ -7,16 +22,16 @@
  */
 int p1(char *s, int l)
 {
-        if (l < 1)
-        {
-                return (1);
-        }
+	if (l < 1)
+	{
+		return (1);
+	}
 
-        if (*s == *(s + l))
-        {
-                return (p1(s + 1, l - 2));
-        }
-        return (0);
+	if (*s == *(s + l))
+	{
+		return (p1(s + 1, l - 2));
+	}
+	return (0);
 }
 
 /**
@@ -26,8 +41,7 @@ int p1(char *s, int l)
  */
 int is_palindrome(char *s)
 {
-        int len = _strlen_recursion(s);
+	int len = _strlen_recursion(s);
 
-        return (p1(s, len - 1));
+	return (p1(s, len - 1));
 }
-
